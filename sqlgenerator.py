f5cb796e29323645369214d33d309b840c679e62
miris-mimitako <miris.mikitako@gmail.com>
2021-05-06 23:16:35 +0900

    Modified: sqlgenerator

--- sqlgenerator.py
+++ sqlgenerator.py
@@ -3,11 +3,18 @@
 ### this file is not completed coding.
 
 class Sql_Generator():
-    def sql_search(self, table_name, *args):
+    def sql_search(self, **kwargs):
         #-------------------------------------------
-        # This def is simple keyword serach on single table.
-        # *args are column key, search key word in WHERE frase
-        # Ex. sample_column = "ex_word" then please input 'sample_column = "ex_word"'
+        # This def is simple keyword serach
+        # Must use dictionary method:
+        #   key: s_column -- when no define, this def can automaticaly choose "*"
+        #       You can input one column or multi comlumns.
+        #       Multi comlumns use list in dictionary.
+        #   key: s_from -- must input your table name or query
+        #   key: s_where -- search keyword(s) in column(s)
+        #       You must use SQL frase
+        # Example:
+        #   s=sql_search({s_column: [username, address], s_from: table_name, s_where: "country='Mexico' and sex = 'female'"})
         # 
         #-------------------------------------------
         print ("start: sql_search")
@@ -16,24 +23,28 @@ class Sql_Generator():
         _column_key = ""
         sql = ""
         n = 0 
-        self.table_name = table_name
-        self.search_word = search_word
+        self.s_from = " FROM " + kwargs["s_from"]
+        self.s_column="SELECT "
 
-        # comumn key word input
-        for i in args:
+        #Where keys input
+        if kwargs.get("s_where") != None:
+            self.s_where=kwargs["s_where"]
+        ## end if
+
+        #column keys input
+        for i in kwargs["s_column"]:
             if n > 1:
-                _column_key += "AND "
+                self.s_column += ", "
+            self.s_column = i
             ## end if
 
-            _column_key += args[n]
             n += 1
-        ## end of for
+        ## end for
 
-        sql = "SELECT"
+        sql = self.s_column + " FROM " + self.s_from + " WHERE " + self.s_where + ";"
         print ("End: sql_search")
 
     ## end of sql_search
-
     
 ## end of class Sql_Generator
 
