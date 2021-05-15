955035aea4fce3e865e859cc642ec5974c4c3cfc
miris-mimitako <miris.mikitako@gmail.com>
2021-05-16 07:53:47 +0900

    modified: s_from to s_table

--- sqlgenerator.py
+++ sqlgenerator.py
@@ -5,12 +5,15 @@
 '''
 This coding must use type of dictionary
 key: s_column---Table / query column name(s) (multiple method you must use list in dictionary)
-key: s_from---Table name 
+key: s_table---Table name 
 key: s_where---search items
 dic_sql_generator={
+    #--- select method ---*
     s_column: 
-    s_from:
+    s_table:
     s_where:
+    #--- insert method ---*
+    
 }
 '''
 
@@ -35,8 +38,8 @@ class Sql_Generator:
         ## end if
 
         #from keys input
-        if self.sql_keys.get("s_from") != None:
-            self.s_from=self.sql_keys["s_from"]
+        if self.sql_keys.get("s_table") != None:
+            self.s_table=self.sql_keys["s_table"]
         ## end if
 
         print ("End: Sql_Generator / __init")
@@ -48,11 +51,11 @@ class Sql_Generator:
         #   key: s_column -- when no define, this def can automaticaly choose "*"
         #       You can input one column or multi comlumns.
         #       Multi comlumns use list in dictionary.
-        #   key: s_from -- must input your table name or query
+        #   key: s_table -- must input your table name or query
         #   key: s_where -- search keyword(s) in column(s)
         #       You must use SQL frase
         # Example:
-        #   s=sql_search({s_column: [username, address], s_from: table_name, s_where: "country='Mexico' and sex = 'female'"})
+        #   s=sql_search({s_column: [username, address], s_table: table_name, s_where: "country='Mexico' and sex = 'female'"})
         # 
         #-------------------------------------------
         print ("start: sql_search")
@@ -61,7 +64,7 @@ class Sql_Generator:
         sql = ""
         n = 0 
 
-        self.s_from = " FROM " + self.s_from
+        self.s_table = " FROM " + self.s_table
 
         # Colum may have list argument
 
@@ -83,13 +86,17 @@ class Sql_Generator:
         ## end if 
 
 
-        sql = _column_key + self.s_from +  self.s_where + ";"
+        sql = _column_key + self.s_table +  self.s_where + ";"
         
         return sql
         
         print ("End: sql_search")
     ## end of sql_search
     
+    def sql_insert(self):
+        pass
+    ## end of sql_insert
+
 ## end of class Sql_Generator
 
 if __name__=="__main__":
